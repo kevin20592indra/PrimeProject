b45659b313dbbfa7224a8ed4f9490681c60e1957
kevinindra <kevin20592indra@alumni.itb.ac.id>
2018-08-24 08:45:57 +0700

    Initial commit

---
+++ PrimeProject.Business/Entity/Items.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeProject.Business.Entity
+{
+    public class Item
+    {
+        public Guid ItemId { get; set; }
+        public string ItemName { get; set; }
+        public string Type { get; set; }
+        public string Parameters { get; set; }
+    }
+}
---
+++ PrimeProject.Business/EntityFramework/PrimeContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+using PrimeProject.Business.Entity;
+
+namespace PrimeProject.Business.EntityFramework
+{
+    public class PrimeContext : DbContext
+    {
+        public DbSet<Item> Items{ get; set; }
+        public PrimeContext() : base("name=PrimeContext")
+        {
+
+        }
+    }
+}
