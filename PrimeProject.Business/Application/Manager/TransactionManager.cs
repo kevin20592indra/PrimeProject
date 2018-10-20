aea849f4f21a2c18a3cdb3fa9f5e68fa9a9f4c95
kevinindra <kevin@xtremax.com>
2018-10-20 17:22:31 +0700

    [Add] Transaction and Stock Model

---
+++ PrimeProject.Business/Application/Manager/TransactionManager.cs
@@ -0,0 +1,21 @@
+﻿using log4net;
+using PrimeProject.Business.Entity;
+
+namespace PrimeProject.Business.Application.Manager
+{
+    public class TransactionManager : ModuleManager<Transaction>
+    {
+        public static ILog log = LogManager.GetLogger(typeof(TransactionManager).Name);
+
+        private static TransactionManager singleton;
+
+        private TransactionManager()
+        {
+        }
+
+        public static TransactionManager GetInstance()
+        {
+            return singleton ?? (singleton = singleton = new TransactionManager());
+        }
+    }
+}
---
+++ PrimeProject.Business/Entity/Transaction.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PrimeProject.Business.Entity
+{
+    public class Transaction
+    {
+        public Guid Id { get; set; }
+        public DateTime TransactionTime { get; set; }
+        public string Type { get; set; }
+        public string Parameters { get; set; }
+        public virtual Item Item { get; set; }
+    }
+}
